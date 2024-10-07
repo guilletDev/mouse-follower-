65bb750bb552818a62e28502dfd1e2cecfc60e5b
guilletdev <guilletdev@gmail.com>
2024-10-07 11:55:21 -0300

    Agrego puntero rendondo y el activador

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,31 @@
-
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  
+  const [enabled, setEnabled] = useState(false)
+
+  useEffect(()=>{
+    console.log(`effect ${enabled}`)
+  },[enabled])
 
   return (
     <>
+      <div style={{
+        position: 'absolute',
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+        border: '1px solid #fff',
+        borderRadius: '50%',
+        opacity: 0.8,
+        pointerEvents: 'none',
+        left: -25,
+        top: -25,
+        width: 50,
+        height: 50,
+        transform: `translate (0px, 0px)`
+        }}
+      />
       <h3>Mouse Follower</h3>
+      <button onClick={()=>{setEnabled(!enabled) }}   >{enabled ? 'Desactivar' : 'Activar'} el puntero</button>
     </>
   )
 }
